6799b8c8f1d667f0b0fd5edb195bcf202b4bfd68
Anjali Tiwari <anjalitiwari@salesforce.com>
2022-02-22 23:42:40 +0100

    Try

--- src/main/java/com/bezkoder/spring/jdbc/postgresql/SpringBootJdbctemplatePostgresqlApplication.java
+++ src/main/java/com/bezkoder/spring/jdbc/postgresql/SpringBootJdbctemplatePostgresqlApplication.java
@@ -9,5 +9,4 @@ public class SpringBootJdbctemplatePostgresqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJdbctemplatePostgresqlApplication.class, args);
 	}
-
 }
